e8d476f5b86063c25532318f27827c20527e74d3
hxgoantoniooo <10032005gonzalez@gmail.com>
2024-10-23 02:30:34 -0300

    Trabajadores v1.0

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template, request
 from flask_mail import Mail, Message
-import datetime
+from datetime import datetime
 
 app = Flask(__name__)
 
-# Configuración de Flask-Mail para Outlook
-app.config['MAIL_SERVER'] = 'smtp.office365.com'
+# Configuraciones de Flask-Mail
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'ejemplotrabajadores@outlook.com'
-app.config['MAIL_PASSWORD'] = '26062016Hg!'
+app.config['MAIL_USERNAME'] = '10032005gonzalez@gmail.com'
+app.config['MAIL_PASSWORD'] = 'hspa odbg pwxe rnqx'
+app.config['MAIL_DEFAULT_SENDER'] = '10032005gonzalez@gmail.com'
+
 mail = Mail(app)
 
 @app.route('/')
@@ -18,21 +20,17 @@ def index():
 
 @app.route('/send-email', methods=['POST'])
 def send_email():
-    try:
-        numero = request.form['numero']
-        dia_actual = datetime.datetime.now().strftime("%d/%m/%Y")
-        mensaje = f"Hola, hoy {dia_actual} fueron {numero} personas.\nSaludos!"
-        
-        msg = Message('Reporte de Personas', 
-                      sender='ejemplotrabajadores@outlook.com', 
-                      recipients=['10032005gonzalez@gmail.com'])
-        msg.body = mensaje
-        
-        mail.send(msg)
-        return 'Correo enviado exitosamente'
-    except Exception as e:
-        print(f"Error al enviar el correo: {e}")
-        return 'Error interno del servidor. Inténtalo nuevamente más tarde.', 500
+    numero_personas = request.form['numero_personas']
+    destinatario = "hugonzalezcontreras@gmail.com"  # Destinatario fijo
+    fecha = datetime.now().strftime('%Y-%m-%d')
+
+    mensaje = f"Hoy {fecha} fueron {numero_personas} personas,\nSaludos"
+
+    msg = Message("Informe Diario", recipients=[destinatario])
+    msg.body = mensaje
+
+    mail.send(msg)
+    return "Correo enviado exitosamente!"
 
 if __name__ == '__main__':
     app.run(debug=True)
